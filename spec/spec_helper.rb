58c9ee09899a764ba3c6808d64839dad9e79b4d1
Fatpaher <fatpaher@gmail.com>
2016-04-23 16:30:56 +0300

    Add Factorygirl.lint
    Remove FactoryGirl syntax config from spec_helper.rb to support/factory_girl.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,8 @@ require_relative 'support/controller_helpers'
 require 'devise'
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
   config.include ControllerHelpers, type: :controller
-  
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
