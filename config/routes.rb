292c06483643bee0583c695cfef9d6cd306291bd
xuliaP <paukov2967@gmail.com>
2016-06-03 19:33:59 +0300

    add rails admin
    add kaminari remove willpaginate
    
    locale rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'oauths/vkontakte'
   post 'toggle_group/:id' => 'groups#toggle_group', as: :toggle_group
   root 'pages#index'
