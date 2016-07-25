2a3cd2b15bdc9a2829d2ede556aaae73a27c8159
Badichan Dmitriy <badimalex@gmail.com>
2016-07-25 15:25:54 +0300

    Search by city name

--- spec/features/search_purchases_spec.rb
+++ spec/features/search_purchases_spec.rb
@@ -1,8 +1,10 @@
 require 'acceptance_helper'
 
 feature 'Search purchases' do
+  given!(:city) { create(:city, name: 'Moscow') }
+  given!(:group) { create(:group, city: city) }
   given!(:purchases) { create_list(:purchase, 2) }
-  given!(:purchase) { create(:purchase) }
+  given!(:purchase) { create(:purchase, group: group) }
 
   before do
     purchases
@@ -14,5 +16,12 @@ feature 'Search purchases' do
     click_on 'search'
     expect(page).to have_link(purchase.name)
     expect(page).to_not have_link(purchases.first.name)
+  end
+
+  scenario 'by city name', js: true do
+    fill_in 'grid_f_name', with: city.name
+    click_on 'search'
+    expect(page).to have_content(city.name)
+    expect(page).to_not have_link(purchases.first.city.name)
   end
 end
