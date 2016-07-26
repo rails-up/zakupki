f936b2278fc85897cd69221d2ece6174e4189f78
Badichan Dmitriy <badimalex@gmail.com>
2016-07-26 19:48:48 +0300

    Improvements

--- spec/features/search_purchases_spec.rb
+++ spec/features/search_purchases_spec.rb
@@ -2,9 +2,9 @@ require 'acceptance_helper'
 
 feature 'Search purchases' do
   given!(:city) { create(:city, name: 'Moscow') }
-  given!(:group) { create(:group, enabled: true) }
+  given!(:group) { create(:group, city: city, enabled: true) }
   given!(:purchases) { create_list(:purchase, 2, group: create(:group, enabled: true)) }
-  given!(:purchase) { create(:purchase, city: city, name: 'Unique purchase') }
+  given!(:purchase) { create(:purchase, city: city) }
   given!(:purchase_without_group) { create(:purchase, group: nil) }
 
   before do
@@ -12,18 +12,18 @@ feature 'Search purchases' do
     visit purchases_path
   end
 
+  scenario 'by name', js: true do
+    fill_in 'grid_f_name', with: purchase.name
+    click_on 'search'
+    expect(page).to have_link(purchase.name)
+    expect(page).to_not have_link(purchases.first.name)
+  end
+
   scenario 'by city name', js: true do
     fill_in 'grid_f_cities_name', with: city.name
     click_on 'search'
     expect(page).to have_content(city.name)
     expect(page).to_not have_link(purchases.first.city.name)
-  end
-
-  xscenario 'by name', js: true do
-    fill_in 'grid_f_name', with: purchase.name
-    click_on 'search'
-    expect(page).to have_link(purchase.name)
-    expect(page).to_not have_link(purchases.first.name)
   end
 
   context 'by group' do
